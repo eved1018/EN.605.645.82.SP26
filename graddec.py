0030578e117a2caa732c73d8b73837e5794d1cf6
eved1018 <edelsteinevan@gmail.com>
2026-02-03 08:48:47 -0500

    update

--- graddec.py
+++ graddec.py
@@ -1,5 +1,8 @@
 import random
+from math import exp, log10
 
+def slog(x):
+    return log10(x) if x > 0 else 0
 
 def calc_error(thetas, ps, ys):
     # yhats = [0] * len(ps)
@@ -13,6 +16,7 @@ def calc_error(thetas, ps, ys):
         error += (yh - y) ** 2
     return error / (2 * len(ys)), yhats
 
+
 # def update_theta(thetas, ps, ys, yhats, alpha):
 #     n = len(ys)
 #     ds = [(yh - y) for yh, y in zip(yhats, ys)]
@@ -23,6 +27,7 @@ def calc_error(thetas, ps, ys):
 #
 #     js = [(alpha * i/n) for i in js]
 #     return [t - j for t,j in zip(thetas, js)]
+
 
 def update_theta(thetas, ps, ys, yhats, alpha):
     n = len(ys)
@@ -42,13 +47,14 @@ def linreg(ps: list[list[float]], ys: list[float], epsilon: float, alpha: float)
 
     prev_error = 0.0
     cur_error, yhats = calc_error(thetas, ps, ys)
-    
+
     while abs(cur_error - prev_error) >= epsilon:
         thetas = update_theta(thetas, ps, ys, yhats, alpha)
         prev_error = cur_error
         cur_error, yhats = calc_error(thetas, ps, ys)
 
     return thetas
+
 
 def calc_log_error(thetas, ps, ys):
     # yhats = [0] * len(ps)
@@ -57,17 +63,28 @@ def calc_log_error(thetas, ps, ys):
     #         yhats[c] += t * x
 
     yhats = [sum(t * x for t, x in zip(thetas, xs)) for xs in ps]
+    yhats = [1/(1 + exp(-1 * z)) for z in yhats]
     error = 0
     for yh, y in zip(yhats, ys):
-        error += (yh - y) ** 2
-    return error / (2 * len(ys)), yhats
+        # error += (yh - y) ** 2
+        de =  y * slog(yh) + (1-y) * slog(1 - yh)
+        error += de
+        # error += y * slog(yh) + (1-y) * slog(1 - yh)
+    error = (-1.0/ len(ys)) * error
+    return error, yhats
+
+
 def update_log_theta(thetas, ps, ys, yhats, alpha):
     n = len(ys)
     ds = [(yh - y) for yh, y in zip(yhats, ys)]
     js = [sum(x * d for x, d in zip(xs, ds)) for xs in ps]
     return [t - alpha * j / n for t, j in zip(thetas, js)]
 
+
 def logreg(ps: list[list[float]], ys: list[float], epsilon: float, alpha: float):
+
+    state = []
+
     for i in ps:
         i.insert(0, 1.0)
 
@@ -75,17 +92,27 @@ def logreg(ps: list[list[float]], ys: list[float], epsilon: float, alpha: float)
 
     prev_error = 0.0
     cur_error, yhats = calc_log_error(thetas, ps, ys)
-    
+    state.append([thetas, yhats, cur_error])
+
     while abs(cur_error - prev_error) >= epsilon:
         thetas = update_log_theta(thetas, ps, ys, yhats, alpha)
         prev_error = cur_error
-        cur_error, yhats = calc_error(thetas, ps, ys)
+        cur_error, yhats = calc_log_error(thetas, ps, ys)
+        state.append([thetas, yhats, cur_error])
+        break
+    
+    print(*state,sep="\n")
 
     return thetas
 
 
+# ps = [[1.0], [3.0]]
+# ys = [2.0, 1.0]
+# thetas = linreg(ps, ys, 1 * 10**-7, 0.1)
+# print(thetas)
+#
 
-ps = [[1.0], [3.0]]
-ys = [2.0, 1.0]
-thetas = linreg(ps, ys, 1*10**-7, 0.1)
+ps = [[1.1], [2.7]]
+ys = [0,1]
+thetas = logreg(ps, ys, 1 * 10**-7, 0.1)
 print(thetas)
