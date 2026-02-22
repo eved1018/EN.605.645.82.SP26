fb4f462b294883b22bea148c4ca7c50df812a50e
ev1018 <edelsteinevan@gmail.com>
2026-02-21 23:00:45 -0500

    update

--- algs/seds.py
+++ algs/seds.py
@@ -1,5 +1,22 @@
 from typing import List, Set, Tuple
 
+"""
+The goal of this module is to implemend succesive iteration of dominant stratagies.
+It takes in normal form game and wether to use strong or weakly dominant elimination
+For striclty dominant elimination it returns the Nash Equilibrium stratagies,
+for weakly dominant eliminiation it returns all possible Nash Equilibrium
+
+The basics of the SEDS algorithm are straightforward.
+Consider any two of my available strategies. If the payoff from strategy A is greater than the payoff from
+strategy B no matter what my opponent does, Im never going to be pick Strategy B. In such a case,
+Strategy A strongly dominates Strategy B and the dominated strategy can be eliminated from
+consideration. Essentially, the range of strategies I need to consider just got smaller. Yay.
+It may be the case that some of the payoffs for Strategies A and B are equal for a given strategy of the
+opponent. If at least one is better than we can say Strategy A weakly dominates Strategy B and we
+can still eliminate Strategy B from consideration.
+
+This module uses BFS and state space search to search through all possible elmination paths
+"""
 
 def is_strongly_dominated(
     payoffs_c: List[int], payoffs_j: List[int]
@@ -25,19 +42,15 @@ def get_payoffs(
     game: List[List[Tuple[int, int]]],
     player: int,
     strategy: int,
-    oppenent_indices: Tuple[int, ...],
+    opponent_indices: Tuple[int, ...],
 ) -> List[int]:
-    payoffs = []
     if player == 0:
-        payoffs = [game[strategy][c][player] for c in oppenent_indices]
+        return [game[strategy][c][player] for c in opponent_indices]
 
-    elif player == 1:
-        payoffs = [game[r][strategy][player] for r in oppenent_indices]
-
-    return payoffs
+    return [game[r][strategy][player] for r in opponent_indices]
 
 
-def get_dominated(
+def dominated_strategies(
     game: List[List[Tuple[int, int]]],
     player: int,
     row_indices: Tuple[int, ...],
@@ -45,56 +58,49 @@ def get_dominated(
     weak: bool,
 ) -> Set[int]:
     indices = row_indices if player == 0 else col_indices
-    oppenent_indices = col_indices if player == 0 else row_indices
-
-    dominated_stratagies = set()
+    opponent_indices = col_indices if player == 0 else row_indices
+    dominated = set()
 
     for c in indices:
-        payoffs_c = get_payoffs(game, player, c, oppenent_indices)
+        payoffs_c = get_payoffs(game, player, c, opponent_indices)
 
         for j in indices:
             if j == c:
                 continue
-            payoffs_j = get_payoffs(game, player, j, oppenent_indices)
+
+            payoffs_j = get_payoffs(game, player, j, opponent_indices)
+
             if is_dominated(payoffs_c, payoffs_j, weak):
-                print(player,c, j,  payoffs_c, payoffs_j)
-                dominated_stratagies.add(c)
+                dominated.add(c)
                 break
 
-    return dominated_stratagies
+    return dominated
 
 
-def remove_strategy(indices, index):
-    return tuple(i for i in indices if i != index)
+def remove_one_strategy(indices: Tuple[int, ...], remove_index: int) -> Tuple[int, ...]:
+    return tuple(i for i in indices if i != remove_index)
 
 
-def succesors(
+def successors(
     game: List[List[Tuple[int, int]]],
     strategies: Tuple[Tuple[int, ...], Tuple[int, ...]],
     weak: bool,
-) -> List[
-    Tuple[Tuple[int, ...], Tuple[int, ...]]
-]:  # return stategies to be searched after removal of dominated stategies
-    valid_rows = strategies[0]
-    valid_cols = strategies[1]
+) -> List[Tuple[Tuple[int, ...], Tuple[int, ...]]]:
+    valid_rows, valid_cols = strategies
+    successor_strategies = []
 
-    succesor_strategies = []
-
-    dominated_rows = get_dominated(game, 0, valid_rows, valid_cols, weak)
+    dominated_rows = dominated_strategies(game, 0, valid_rows, valid_cols, weak)
     for row in dominated_rows:
-        s = remove_strategy(valid_rows, row)
-        succesor_strategies.append((s, valid_cols))
+        successor_strategies.append((remove_one_strategy(valid_rows, row), valid_cols))
 
-    dominated_cols = get_dominated(game, 1, valid_rows, valid_cols, weak)
+    dominated_cols = dominated_strategies(game, 1, valid_rows, valid_cols, weak)
     for col in dominated_cols:
-        s = remove_strategy(valid_cols, col)
-        succesor_strategies.append((valid_rows, s))
-    return succesor_strategies
+        successor_strategies.append((valid_rows, remove_one_strategy(valid_cols, col)))
+
+    return successor_strategies
 
 
-def search(
-    game: List[List[Tuple[int, int]]], weak: bool = False
-) -> List[Tuple[int, int]]:  # BFS over all possible elminiations
+def search(game: List[List[Tuple[int, int]]], weak: bool = False) -> List[Tuple[int, int]]:  # BFS over all possible elminiations
     rows = len(game)
     cols = len(game[0])
 
@@ -106,19 +112,13 @@ def search(
     frontier: Set[Tuple[Tuple[int, ...], Tuple[int, ...]]] = {strategies}
     visited: Set[Tuple[Tuple[int, ...], Tuple[int, ...]]] = set()
 
-    depth = 0
     while frontier:
-        strategies = frontier.pop()
-
-        print(f"Depth {depth} | stratagies {strategies}")
+        strategies = frontier.pop(0)
         visited.add(strategies)
 
-        depth += 1
+        next_strategies = successors(game, strategies, weak)
 
-        next_strategies = succesors(game, strategies, weak)
-
-        # if is_goal_state(strategies):
-        if not next_strategies:  # no more eliminated stratagies
+        if not next_strategies:
             for i in strategies[0]:
                 for j in strategies[1]:
                     solutions.add((i, j))
@@ -140,8 +140,38 @@ game1 = [
 
 eql = search(game1, False)
 print()
-print("game 1:  ", eql)
-# assert eql == [(1, 1)]
+print(eql)
+assert eql == [(1, 1)]
+
+game2 = [[(3, 0), (2, 1), (0, 0)], [(1, 1), (1, 1), (5, 0)], [(0, 1), (4, 2), (0, 1)]]
+print()
+eql = search(game2)
+print()
+print(eql)
+assert eql == [(2, 1)]
+
+game3 = [
+    [(1, 0), (3, 1), (1, 1)],
+    [(1, 1), (3, 0), (0, 1)],
+    [(2, 2), (3, 3), (0, 2)],
+]
+
+
+print()
+eql = search(game3, True)
+print()
+print(eql)
+
+
+prisoners_dilemma = [
+ [( -5, -5), (-1,-10)],
+ [(-10, -1), (-2, -2)]]
+
+print()
+eql = search(prisoners_dilemma, True)
+print()
+print(eql)
+assert eql == [(0,0)]
 
 #
 # game2 = [[(3, 0), (2, 1), (0, 0)], [(1, 1), (1, 1), (5, 0)], [(0, 1), (4, 2), (0, 1)]]
